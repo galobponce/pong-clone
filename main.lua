27a4adc1fcb19099cf1f45619a1a3e02ae9a2286
galobponce <personal.galo.bello@gmail.com>
2022-03-26 09:19:14 -0500

    format

--- main.lua
+++ main.lua
@@ -13,262 +13,257 @@ require 'src/classes/Paddle'
 GAME_WIDTH, GAME_HEIGHT = 432, 243
 
 local WINDOW_WIDTH, WINDOW_HEIGHT = love.window.getDesktopDimensions()
-local WINDOW_WIDTH, WINDOW_HEIGHT = WINDOW_WIDTH *.8, WINDOW_HEIGHT *.8
+local WINDOW_WIDTH, WINDOW_HEIGHT = WINDOW_WIDTH * .8, WINDOW_HEIGHT * .8
 
 -- Ran only once to initialize the game
 function love.load()
-  love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.graphics.setDefaultFilter('nearest', 'nearest')
 
-  -- Retro-looking font
-  smallFont = love.graphics.newFont('src/fonts/retro-font.ttf', 8)
-  largeFont = love.graphics.newFont('src/fonts/retro-font.ttf', 32)
+    -- Retro-looking font
+    smallFont = love.graphics.newFont('src/fonts/retro-font.ttf', 8)
+    largeFont = love.graphics.newFont('src/fonts/retro-font.ttf', 32)
 
-  -- Random numbers.
-  math.randomseed(os.time())
+    -- Random numbers.
+    math.randomseed(os.time())
 
-  sounds = {
-    ['background'] = love.audio.newSource('src/sounds/background-music.wav', 'stream'),
-    ['serve'] = love.audio.newSource('src/sounds/serve.wav', 'static'),
-    ['paddle-hit'] = love.audio.newSource('src/sounds/paddle_hit.wav', 'static'),
-    ['wall-hit'] = love.audio.newSource('src/sounds/wall_hit.wav', 'static'),
-    ['score'] = love.audio.newSource('src/sounds/score.wav', 'static')
-  }
-  -- Background music
-  sounds['background']:setLooping(true)
-  sounds['background']:setVolume(0.8)
-  sounds['background']:play()
+    sounds = {
+        ['background'] = love.audio.newSource('src/sounds/background-music.wav', 'stream'),
+        ['serve'] = love.audio.newSource('src/sounds/serve.wav', 'static'),
+        ['paddle-hit'] = love.audio.newSource('src/sounds/paddle_hit.wav', 'static'),
+        ['wall-hit'] = love.audio.newSource('src/sounds/wall_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('src/sounds/score.wav', 'static')
+    }
+    -- Background music
+    sounds['background']:setLooping(true)
+    sounds['background']:setVolume(0.8)
+    sounds['background']:play()
 
-  --[[
+    --[[
     Object's intance
   ]]
 
-  player1 = Paddle(10, 30, 5, 20, 200)
-  player2 = Paddle(GAME_WIDTH - 10, GAME_HEIGHT - 50, 5, 20, 200)
-  ball = Ball(GAME_WIDTH / 2 - 2, GAME_HEIGHT / 2 - 2, 4, 4)
+    player1 = Paddle(10, 30, 5, 20, 200)
+    player2 = Paddle(GAME_WIDTH - 10, GAME_HEIGHT - 50, 5, 20, 200)
+    ball = Ball(GAME_WIDTH / 2 - 2, GAME_HEIGHT / 2 - 2, 4, 4)
 
-  --[[
+    --[[
     Sets game's state; can be any of the following:
     1. 'menu' : (beggining of the game, before serve)
     2. 'serve' : (waiting to play key pressed to serve the ball)
     3. 'play' : (the is in play)
     4. 'win' : (the game is over, with a winner, ready for restart)
   ]]
-  gameState = 'menu'
+    gameState = 'menu'
 
-  -- Defines if player is playing against the machine
-  againtsMachine = false
+    -- Defines if player is playing against the machine
+    againtsMachine = false
 
-  -- Configure push lib
-  push:setupScreen(GAME_WIDTH, GAME_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
-    fullscreen = false,
-    rezisable = false,
-    vsync = true
-  })
+    -- Configure push lib
+    push:setupScreen(GAME_WIDTH, GAME_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        rezisable = false,
+        vsync = true
+    })
 end
-
 
 -- Keyboard handling.
 function love.keypressed(key)
-  if key == 'escape' then
-    love.event.quit()
+    if key == 'escape' then
+        love.event.quit()
 
-  -- Toggle between states
-  elseif key == 'enter' or key == 'return' then
-    if gameState == 'menu' then
-      -- Player to serve is random
-      playerToServe = math.random(2) == 1 and 1 or 2
-      player1:resetScore()
-      player2:resetScore()
-      gameState = 'serve'
-    elseif gameState == 'serve' then
-      -- Serves the ball and plays sound
-      ball:serve()
-      sounds['serve']:play()
-      gameState = 'play'
-    elseif gameState == 'win' then
-      -- Same as menu to serve
-      playerToServe = math.random(2) == 1 and 1 or 2
-      player1:resetScore()
-      player2:resetScore()
-      gameState = 'serve'
+        -- Toggle between states
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'menu' then
+            -- Player to serve is random
+            playerToServe = math.random(2) == 1 and 1 or 2
+            player1:resetScore()
+            player2:resetScore()
+            gameState = 'serve'
+        elseif gameState == 'serve' then
+            -- Serves the ball and plays sound
+            ball:serve()
+            sounds['serve']:play()
+            gameState = 'play'
+        elseif gameState == 'win' then
+            -- Same as menu to serve
+            playerToServe = math.random(2) == 1 and 1 or 2
+            player1:resetScore()
+            player2:resetScore()
+            gameState = 'serve'
+        end
+    elseif key == 'lshift' or key == 'rshift' then
+        if gameState == 'menu' then
+            -- Player to serve is user
+            againtsMachine = true
+            playerToServe = 1
+            player1:resetScore()
+            player2:resetScore()
+            gameState = 'serve'
+        end
     end
-  elseif key == 'lshift' or key == 'rshift' then
-		if gameState == 'menu' then
-		-- Player to serve is user
-		againtsMachine = true
-		playerToServe = 1
-		player1:resetScore()
-		player2:resetScore()
-		gameState = 'serve'
-		end
-  end
 end
-
 
 -- Runs every frame with 'dt' (1/6 of second) passed in
 function love.update(dt)
-  if gameState == 'play' then
-    -- Player 1 movement
-    if love.keyboard.isDown('w') then
-      player1.velocity = -player1.speed
-    elseif love.keyboard.isDown('s') then
-      player1.velocity = player1.speed
-    else
-      player1.velocity = 0
-    end
+    if gameState == 'play' then
+        -- Player 1 movement
+        if love.keyboard.isDown('w') then
+            player1.velocity = -player1.speed
+        elseif love.keyboard.isDown('s') then
+            player1.velocity = player1.speed
+        else
+            player1.velocity = 0
+        end
 
-    -- Player 2 movement
-    if love.keyboard.isDown('up') and not againtsMachine then
-      player2.velocity = -player2.speed
-    elseif love.keyboard.isDown('down') and not againtsMachine then
-      player2.velocity = player2.speed
-    else
-      player2.velocity = 0
-    end
+        -- Player 2 movement
+        if love.keyboard.isDown('up') and not againtsMachine then
+            player2.velocity = -player2.speed
+        elseif love.keyboard.isDown('down') and not againtsMachine then
+            player2.velocity = player2.speed
+        else
+            player2.velocity = 0
+        end
 
-		-- Machine movement
-		if againtsMachine then
-			player2.y = ball.y
-		end
+        -- Machine movement
+        if againtsMachine then
+            player2.y = ball.y
+        end
 
-    --[[
+        --[[
       Collision with top & bottom screen
     ]]
-    -- Top of the screen
-    if ball.y <= 0 then
-      ball.y = 0
-      ball.velocityInY = -ball.velocityInY
-      sounds['wall-hit']:play()
-    end
+        -- Top of the screen
+        if ball.y <= 0 then
+            ball.y = 0
+            ball.velocityInY = -ball.velocityInY
+            sounds['wall-hit']:play()
+        end
 
-    -- Bottom of the screen
-    if ball.y >= GAME_HEIGHT - 4 then
-      ball.y = GAME_HEIGHT - 4
-      ball.velocityInY = -ball.velocityInY
-      sounds['wall-hit']:play()
-    end
+        -- Bottom of the screen
+        if ball.y >= GAME_HEIGHT - 4 then
+            ball.y = GAME_HEIGHT - 4
+            ball.velocityInY = -ball.velocityInY
+            sounds['wall-hit']:play()
+        end
 
-    --[[
+        --[[
       Collision with left & right screen
     ]]
-    -- Left side
-    if ball.x < 0 then
-      sounds['score']:play()
-      player1:resetPosition(10, 30)
-      player2:resetPosition(GAME_WIDTH - 10, GAME_HEIGHT - 50)
-      player2.__score = player2.__score + 1
+        -- Left side
+        if ball.x < 0 then
+            sounds['score']:play()
+            player1:resetPosition(10, 30)
+            player2:resetPosition(GAME_WIDTH - 10, GAME_HEIGHT - 50)
+            player2.__score = player2.__score + 1
 
-      -- Checks winner condition
-      if player2.__score == 5 then
-        gameState = 'win'
-        ball:resetPosition()
-      else
-        playerToServe = 1
-        gameState = 'serve'
-        ball:resetPosition()
-      end
-    end
+            -- Checks winner condition
+            if player2.__score == 5 then
+                gameState = 'win'
+                ball:resetPosition()
+            else
+                playerToServe = 1
+                gameState = 'serve'
+                ball:resetPosition()
+            end
+        end
 
-    -- Right side
-    if ball.x > GAME_WIDTH then
-      sounds['score']:play()
-      player1:resetPosition(10, 30)
-      player2:resetPosition(GAME_WIDTH - 10, GAME_HEIGHT - 50)
-      player1.__score = player1.__score + 1
+        -- Right side
+        if ball.x > GAME_WIDTH then
+            sounds['score']:play()
+            player1:resetPosition(10, 30)
+            player2:resetPosition(GAME_WIDTH - 10, GAME_HEIGHT - 50)
+            player1.__score = player1.__score + 1
 
-      -- Checks winner condition
-      if player1.__score == 5 then
-        gameState = 'win'
-        ball:resetPosition()
-      else
-        playerToServe = 2
-        gameState = 'serve'
-        ball:resetPosition()
-      end
-    end
+            -- Checks winner condition
+            if player1.__score == 5 then
+                gameState = 'win'
+                ball:resetPosition()
+            else
+                playerToServe = 2
+                gameState = 'serve'
+                ball:resetPosition()
+            end
+        end
 
-    --[[
+        --[[
       Collision with paddles
     ]]
-    -- Player 1
-    if ball:isColliding(player1) then
-      -- Places the ball to not overlap the paddle
-      ball.x = player1.x + player1.width + 3
-      -- Bounces the ball to the other side with more speed (max speed)
-      if (not (ball.velocityInX < -280)) then
-        ball.velocityInX = -ball.velocityInX * 1.12
-      else
-        ball.velocityInX = -ball.velocityInX
-      end
-      sounds['paddle-hit']:play()
+        -- Player 1
+        if ball:isColliding(player1) then
+            -- Places the ball to not overlap the paddle
+            ball.x = player1.x + player1.width + 3
+            ball.velocityInX = -ball.velocityInX * 1.12
+            sounds['paddle-hit']:play()
+        end
+
+        -- Player 2
+        if ball:isColliding(player2) then
+            ball.x = player2.x - player2.width - 3
+
+            if ball:isCollidingTop(player2) then
+
+            elseif ball:isCollidingBottom(player2) then
+
+            end
+
+            ball.velocityInX = -ball.velocityInX * 1.12
+            sounds['paddle-hit']:play()
+        end
+
+        -- Update paddle's position
+        player1:update(dt)
+        player2:update(dt)
+
+        -- Update ball's Movement
+        ball:update(dt)
     end
-
-    -- Player 2
-    if ball:isColliding(player2) then
-      ball.x = player2.x - player2.width - 3
-      if (not (ball.velocityInX > 280)) then
-        ball.velocityInX = -ball.velocityInX * 1.12
-      else
-        ball.velocityInX = -ball.velocityInX
-      end
-      sounds['paddle-hit']:play()
-    end
-
-    -- Update paddle's position
-    player1:update(dt)
-    player2:update(dt)
-
-    -- Update ball's Movement
-    ball:update(dt)
-  end
 end
-
 
 -- Renders anything on screen, called after update.
 function love.draw(input)
-  push:start()
-  -- Background color
-  love.graphics.clear(30/255, 35/255, 45/255, 255/255)
+    push:start()
+    -- Background color
+    love.graphics.clear(30 / 255, 35 / 255, 45 / 255, 255 / 255)
 
-  if gameState == 'menu' then
-    -- UI messages
-    love.graphics.setFont(smallFont)
-    love.graphics.printf('Player 1: W-S', 10, 10, GAME_WIDTH, 'left')
-    love.graphics.printf('Player 2: UP-DOWN', 10, 30, GAME_WIDTH, 'left')
-    love.graphics.printf('Press Enter to play', 10, 50, GAME_WIDTH, 'left')
-    love.graphics.printf('Press Shift to play against machine', 10, 70, GAME_WIDTH, 'left')
-    love.graphics.printf('Press Esc to quit', 10, 90, GAME_WIDTH, 'left')
-    love.graphics.setFont(largeFont)
-    love.graphics.printf('Pong!', 0, GAME_HEIGHT / 2 - 25, GAME_WIDTH, 'center')
+    if gameState == 'menu' then
+        -- UI messages
+        love.graphics.setFont(smallFont)
+        love.graphics.printf('Player 1: W-S', 10, 10, GAME_WIDTH, 'left')
+        love.graphics.printf('Player 2: UP-DOWN', 10, 30, GAME_WIDTH, 'left')
+        love.graphics.printf('Press Enter to play', 10, 50, GAME_WIDTH, 'left')
+        love.graphics.printf('Press Shift to play against machine', 10, 70, GAME_WIDTH, 'left')
+        love.graphics.printf('Press Esc to quit', 10, 90, GAME_WIDTH, 'left')
+        love.graphics.setFont(largeFont)
+        love.graphics.printf('Pong!', 0, GAME_HEIGHT / 2 - 25, GAME_WIDTH, 'center')
 
-  elseif gameState == 'serve' then
-    -- UI messages
-    love.graphics.setFont(smallFont)
-    love.graphics.printf('Player ' .. tostring(playerToServe) .. "'s to serve!", 0, 10, GAME_WIDTH, 'center')
-    love.graphics.printf('Press Enter to serve', 0, 30, GAME_WIDTH, 'center')
-    displayScore()
-    -- Objs
-    player1:render()
-    player2:render()
-    ball:render()
+    elseif gameState == 'serve' then
+        -- UI messages
+        love.graphics.setFont(smallFont)
+        love.graphics.printf('Player ' .. tostring(playerToServe) .. "'s to serve!", 0, 10, GAME_WIDTH, 'center')
+        love.graphics.printf('Press Enter to serve', 0, 30, GAME_WIDTH, 'center')
+        displayScore()
+        -- Objs
+        player1:render()
+        player2:render()
+        ball:render()
 
-  elseif gameState == 'play' then
-      player1:render()
-      player2:render()
-      ball:render()
-      displayScore()
+    elseif gameState == 'play' then
+        player1:render()
+        player2:render()
+        ball:render()
+        displayScore()
 
-  elseif gameState == 'win' then
-      -- UI messages
-      love.graphics.setFont(smallFont)
-      love.graphics.printf('Press Enter to restart', 10, 10, GAME_WIDTH, 'left')
-      love.graphics.printf('Press Esc to quit', 10, 30, GAME_WIDTH, 'left')
-      displayScore()
-      displayWinner()
-  end
+    elseif gameState == 'win' then
+        -- UI messages
+        love.graphics.setFont(smallFont)
+        love.graphics.printf('Press Enter to restart', 10, 10, GAME_WIDTH, 'left')
+        love.graphics.printf('Press Esc to quit', 10, 30, GAME_WIDTH, 'left')
+        displayScore()
+        displayWinner()
+    end
 
-  -- Display FPS in all states
-  displayFPS()
+    -- Display FPS in all states
+    displayFPS()
 
-  push:finish()
+    push:finish()
 end
--- src/classes/Paddle.lua
+++ src/classes/Paddle.lua
@@ -1,21 +1,21 @@
-Paddle = Class{}
+Paddle = Class {}
 
 --[[
   Paddle properties
 ]]
 
 function Paddle:init(x, y, width, height, speed)
-  self.x = x
-  self.y = y
-  self.width = width
-  self.height = height
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
 
-  -- Actual velocity (down / up with its speed)
-  self.velocity = 0
-  -- Speed of velocity
-  self.speed = speed
+    -- Actual velocity (down / up with its speed)
+    self.velocity = 0
+    -- Speed of velocity
+    self.speed = speed
 
-  self.__score = 0
+    self.__score = 0
 end
 
 --[[
@@ -24,30 +24,30 @@ end
 
 -- Sets a new position based on its position
 function Paddle:update(dt)
-  -- Checks if paddle tries to go off the screen
-  
-  -- If paddle going up 
-  if self.velocity < 0 then
-    -- New position equals to the max between top of screen and new position.
-    self.y = math.max(0, self.y + self.velocity * dt)
-  
-  -- If paddle going down
-  else
-    -- The same with bottom of the screen
-    self.y = math.min(GAME_HEIGHT - self.height, self.y + self.velocity * dt)
-  end
+    -- Checks if paddle tries to go off the screen
+
+    -- If paddle going up 
+    if self.velocity < 0 then
+        -- New position equals to the max between top of screen and new position.
+        self.y = math.max(0, self.y + self.velocity * dt)
+
+        -- If paddle going down
+    else
+        -- The same with bottom of the screen
+        self.y = math.min(GAME_HEIGHT - self.height, self.y + self.velocity * dt)
+    end
 end
 
 function Paddle:resetPosition(x, y)
-  self.x = x
-  self.y = y
+    self.x = x
+    self.y = y
 end
 
 function Paddle:resetScore()
-  self.__score = 0
+    self.__score = 0
 end
 
 -- Renders on screen
 function Paddle:render()
-  love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
 end
